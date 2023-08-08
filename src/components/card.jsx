cfd916e44f2089c10a3818014af6de8651bf84aa
Azam Gulov <gulovazam@gmail.com>
2023-08-08 13:14:15 +0500

    Add Container and AddNewButton component

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,8 +1,46 @@
-export default function Card({ children, className, title, onSubmit }) {
+function AddNewButton({ onAdd, title }) {
   return (
-    <form className={`form-section ${className}`} onSubmit={onSubmit}>
+    <button key={title} type="button" onClick={onAdd}>
+      {title}
+    </button>
+  );
+}
+
+function Container({ formState, className, onSubmit, children }) {
+  let container;
+
+  const isNewFormActive = formState === 1;
+  const isEditFormActive = formState === 2;
+
+  if (isNewFormActive || isEditFormActive) {
+    container = (
+      <form className={`form-section ${className}`} onSubmit={onSubmit}>
+        {children}
+      </form>
+    );
+  } else {
+    container = <div className={`form-section ${className}`}>{children}</div>;
+  }
+
+  return container;
+}
+
+export default function Card({
+  children,
+  className,
+  title,
+  buttonTitle,
+  onSubmit,
+  formState,
+  onAdd,
+}) {
+  const isAddButtonActive = formState === 0;
+
+  return (
+    <Container formState={formState} className={className} onSubmit={onSubmit}>
       <h2>{title}</h2>
       {children}
-    </form>
+      {isAddButtonActive && <AddNewButton onAdd={onAdd} title={buttonTitle} />}
+    </Container>
   );
 }

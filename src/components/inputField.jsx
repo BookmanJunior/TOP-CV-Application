df04488f17615098c8ba15b7770bb517dc60d442
Azam Gulov <gulovazam@gmail.com>
2023-08-12 21:47:34 +0500

    Update props
    add handleChange method

--- src/components/inputField.jsx
+++ src/components/inputField.jsx
@@ -3,12 +3,26 @@ export default function InputField({
   title,
   placeholder,
   value,
-  onInput,
+  infoState,
+  setInfoState,
+  itemId,
+  propToUpdate,
 }) {
   const name =
     title.split(" ").length >= 2
       ? title.split(" ")[0].toLowerCase() + title.split(" ")[1]
       : title;
+
+  const handleChange = (e, updateProp) => {
+    setInfoState(
+      infoState.map((item) => {
+        if (item.id === itemId) {
+          return { ...item, [updateProp]: e.target.value };
+        }
+        return { ...item };
+      })
+    );
+  };
 
   return (
     <label>
@@ -18,7 +32,7 @@ export default function InputField({
         name={name}
         value={value}
         placeholder={placeholder}
-        onInput={onInput}
+        onInput={(e) => handleChange(e, propToUpdate)}
         required
       />
     </label>

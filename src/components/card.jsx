a89bb1f6766fd0c717be15d83fa1c3b68f3d20c3
Azam Gulov <gulovazam@gmail.com>
2023-08-17 18:32:18 +0500

    code refactor

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,79 +1,7 @@
 import { useState } from "react";
 import { v4 as uuid } from "uuid";
-import InputField from "./inputField";
-import Forms from "./forms";
-
-function AddNewButton({ onAdd, title }) {
-  return (
-    <button key={title} type="button" onClick={onAdd}>
-      {title}
-    </button>
-  );
-}
-
-function DataContainer({
-  info,
-  handleDelete,
-  handleEdit,
-  expandable,
-  handleAdd,
-  buttonTitle,
-}) {
-  return (
-    <>
-      {info.length > 0 && (
-        <ul>
-          {info.map((i) => (
-            <li key={i.id}>
-              <button type="button" onClick={() => handleEdit(i.id)}>
-                {i.name ??
-                  `${i.firstName ?? "First and"} ${i.lastName ?? "Last Name"}`}
-              </button>
-              {expandable && (
-                <button type="button" onClick={() => handleDelete(i.id)}>
-                  Delete
-                </button>
-              )}
-            </li>
-          ))}
-        </ul>
-      )}
-      {expandable && <AddNewButton onAdd={handleAdd} title={buttonTitle} />}
-    </>
-  );
-}
-
-function Container({
-  formState,
-  className,
-  onSubmit,
-  onCancel,
-  children,
-  containerState,
-}) {
-  const isFormOpen = formState === "opened";
-
-  if (isFormOpen) {
-    return (
-      <form
-        className={`form-section ${className}`}
-        onSubmit={onSubmit}
-        aria-expanded={containerState}
-      >
-        {children}
-        <button>Save</button>
-        <button type="button" onClick={onCancel}>
-          Cancel
-        </button>
-      </form>
-    );
-  }
-  return (
-    <div className={`data-section ${className}`} aria-expanded={containerState}>
-      {children}
-    </div>
-  );
-}
+import InputCard from "./inputCard";
+import DataCard from "./dataCard";
 
 export default function Card({
   className,
@@ -100,9 +28,6 @@ export default function Card({
   const [copyOfExistingData, setCopy] = useState(copyOfInitialItem);
 
   const isAddButtonActive = form === "closed";
-  const editItem = information.filter(
-    (item) => itemsId.editItemId === item.id
-  )[0];
 
   const handleSave = (e) => {
     e.preventDefault();
@@ -156,48 +81,30 @@ export default function Card({
   };
 
   return (
-    <Container
-      formState={form}
-      className={className}
-      onSubmit={handleSave}
-      buttonTitle={buttonTitle}
-      containerState={containerState}
-      onCancel={handleCancel}
-    >
-      <div className="container-header">
-        <h2>{title}</h2>
-        <button type="button" onClick={handleOnShow}></button>
-      </div>
-      <div className="content">
-        {isAddButtonActive ? (
-          <DataContainer
-            info={information}
-            handleDelete={handleDelete}
-            handleEdit={handleEdit}
-            expandable={expandable}
-            buttonTitle={buttonTitle}
-            handleAdd={handleAdd}
-          />
-        ) : (
-          Forms[sectionForm].map((input) => (
-            <InputField
-              key={input.title}
-              type={input.type ?? "text"}
-              title={input.title}
-              placeholder={input.placeholder}
-              infoState={information}
-              setInfoState={setInformation}
-              propToUpdate={input.propToUpdate}
-              itemId={
-                itemsId.editItemId === null
-                  ? itemsId.newItemId
-                  : itemsId.editItemId
-              }
-              value={editItem && editItem[input.propToUpdate]}
-            />
-          ))
-        )}
-      </div>
-    </Container>
+    <>
+      {isAddButtonActive ? (
+        <DataCard
+          info={information}
+          onAdd={handleAdd}
+          onDelete={handleDelete}
+          onEdit={handleEdit}
+          expandable={expandable}
+          buttonTitle={buttonTitle}
+          className={className}
+          containerState={containerState}
+        />
+      ) : (
+        <InputCard
+          information={information}
+          setInformation={setInformation}
+          onSubmit={handleSave}
+          onCancel={handleCancel}
+          containerState={containerState}
+          sectionForm={sectionForm}
+          itemsId={itemsId}
+          className={className}
+        />
+      )}
+    </>
   );
 }

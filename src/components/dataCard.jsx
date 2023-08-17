cfe3eb199d8f2e9032c76afe233a4b507402442a
Azam Gulov <gulovazam@gmail.com>
2023-08-17 18:29:58 +0500

    Move DataCard from card component into its own component
    Initial commit

---
+++ src/components/dataCard.jsx
@@ -0,0 +1,41 @@
+export default function DataCard({
+  info,
+  onAdd,
+  onDelete,
+  onEdit,
+  expandable,
+  buttonTitle,
+  className,
+  containerState,
+}) {
+  return (
+    <div className={`data-section ${className}`} aria-expanded={containerState}>
+      {info.length > 0 && (
+        <ul>
+          {info.map((i) => (
+            <li key={i.id}>
+              <button type="button" onClick={() => onEdit(i.id)}>
+                {i.name ??
+                  `${i.firstName ?? "First and"} ${i.lastName ?? "Last Name"}`}
+              </button>
+              {expandable && (
+                <button type="button" onClick={() => onDelete(i.id)}>
+                  Delete
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
+      {expandable && <AddNewButton onAdd={onAdd} title={buttonTitle} />}
+    </div>
+  );
+}
+
+function AddNewButton({ onAdd, title }) {
+  return (
+    <button key={title} type="button" onClick={onAdd}>
+      {title}
+    </button>
+  );
+}

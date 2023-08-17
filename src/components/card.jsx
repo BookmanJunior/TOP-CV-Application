1eece5d0fff1b04933935a0733cbc51ab24f9c27
Azam Gulov <gulovazam@gmail.com>
2023-08-17 16:55:38 +0500

    Combine states from other components into one
    Code refactor

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+import InputField from "./inputField";
+import Forms from "./forms";
+
 function AddNewButton({ onAdd, title }) {
   return (
     <button key={title} type="button" onClick={onAdd}>
@@ -6,22 +11,35 @@ function AddNewButton({ onAdd, title }) {
   );
 }
 
-function DataContainer({ info, handleDelete, handleEdit }) {
+function DataContainer({
+  info,
+  handleDelete,
+  handleEdit,
+  expandable,
+  handleAdd,
+  buttonTitle,
+}) {
   return (
-    info.length > 0 && (
-      <ul>
-        {info.map((i) => (
-          <li key={i.id}>
-            <button type="button" onClick={() => handleEdit(i.id)}>
-              {i.name}
-            </button>
-            <button type="button" onClick={() => handleDelete(i.id)}>
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    )
+    <>
+      {info.length > 0 && (
+        <ul>
+          {info.map((i) => (
+            <li key={i.id}>
+              <button type="button" onClick={() => handleEdit(i.id)}>
+                {i.name ??
+                  `${i.firstName ?? "First and"} ${i.lastName ?? "Last Name"}`}
+              </button>
+              {expandable && (
+                <button type="button" onClick={() => handleDelete(i.id)}>
+                  Delete
+                </button>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
+      {expandable && <AddNewButton onAdd={handleAdd} title={buttonTitle} />}
+    </>
   );
 }
 
@@ -29,13 +47,13 @@ function Container({
   formState,
   className,
   onSubmit,
+  onCancel,
   children,
   containerState,
 }) {
-  const isNewFormActive = formState === 1;
-  const isEditFormActive = formState === 2;
+  const isFormOpen = formState === "opened";
 
-  if (isNewFormActive || isEditFormActive) {
+  if (isFormOpen) {
     return (
       <form
         className={`form-section ${className}`}
@@ -43,6 +61,10 @@ function Container({
         aria-expanded={containerState}
       >
         {children}
+        <button>Save</button>
+        <button type="button" onClick={onCancel}>
+          Cancel
+        </button>
       </form>
     );
   }
@@ -54,47 +76,126 @@ function Container({
 }
 
 export default function Card({
-  children,
   className,
   title,
   buttonTitle,
-  onSubmit,
-  formState,
-  infoState,
-  containerState = "false",
-  onShow,
-  expandable,
-  handleDelete,
-  handleEdit,
-  onAdd,
+  information,
+  setInformation,
+  containerIndex,
+  setContainerState,
+  sectionForm,
+  initialFormState = "closed",
+  containerState = false,
+  initialItem = null,
+  expandable = true,
 }) {
-  const isAddButtonActive = formState === 0;
+  const [itemsId, setItemsId] = useState({
+    editItemId: initialItem,
+    newItemId: null,
+  });
+  const [form, setActiveForm] = useState(initialFormState);
+
+  const copyOfInitialItem =
+    initialItem && information.filter((item) => item.id === initialItem)[0];
+  const [copyOfExistingData, setCopy] = useState(copyOfInitialItem);
+
+  const isAddButtonActive = form === "closed";
+  const editItem = information.filter(
+    (item) => itemsId.editItemId === item.id
+  )[0];
+
+  const handleSave = (e) => {
+    e.preventDefault();
+
+    setItemsId({ editItemId: null, newItemId: null });
+    setActiveForm("closed");
+  };
+
+  const handleCancel = () => {
+    // check if new item was added, if cancelled remove it
+    if (itemsId.newItemId) {
+      setInformation([
+        ...information.filter((item) => item.id !== itemsId.newItemId),
+      ]);
+    } else if (itemsId.editItemId) {
+      // restore original state from a copy if edit was cancelled
+      setInformation(
+        information.map((item) => {
+          if (item.id === itemsId.editItemId) {
+            return { ...copyOfExistingData };
+          }
+          return { ...item };
+        })
+      );
+    }
+    setItemsId({ editItemId: null, newItemId: null });
+    setActiveForm("closed");
+  };
+
+  const handleEdit = (editItemId) => {
+    const itemToEdit = information.filter((item) => item.id === editItemId)[0];
+    setItemsId({ ...itemsId, editItemId: itemToEdit.id });
+    setCopy({ ...itemToEdit });
+    setActiveForm("opened");
+  };
+
+  const handleAdd = () => {
+    const id = uuid();
+    setInformation([...information, { id }]);
+    setItemsId({ ...itemsId, newItemId: id });
+    setActiveForm("opened");
+  };
+
+  const handleDelete = (id) => {
+    setInformation([...information.filter((key) => key.id !== id)]);
+  };
+
+  const handleOnShow = () => {
+    const newContainerState = containerState ? null : containerIndex;
+    setContainerState(newContainerState);
+  };
 
   return (
     <Container
-      formState={formState}
+      formState={form}
       className={className}
-      onSubmit={onSubmit}
+      onSubmit={handleSave}
       buttonTitle={buttonTitle}
-      onAdd={onAdd}
       containerState={containerState}
+      onCancel={handleCancel}
     >
       <div className="container-header">
         <h2>{title}</h2>
-        {expandable && <button type="button" onClick={onShow}></button>}
+        <button type="button" onClick={handleOnShow}></button>
       </div>
       <div className="content">
         {isAddButtonActive ? (
-          <>
-            <DataContainer
-              info={infoState}
-              handleDelete={handleDelete}
-              handleEdit={handleEdit}
-            />
-            <AddNewButton onAdd={onAdd} title={buttonTitle} />
-          </>
+          <DataContainer
+            info={information}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+            expandable={expandable}
+            buttonTitle={buttonTitle}
+            handleAdd={handleAdd}
+          />
         ) : (
-          children
+          Forms[sectionForm].map((input) => (
+            <InputField
+              key={input.title}
+              type={input.type ?? "text"}
+              title={input.title}
+              placeholder={input.placeholder}
+              infoState={information}
+              setInfoState={setInformation}
+              propToUpdate={input.propToUpdate}
+              itemId={
+                itemsId.editItemId === null
+                  ? itemsId.newItemId
+                  : itemsId.editItemId
+              }
+              value={editItem && editItem[input.propToUpdate]}
+            />
+          ))
         )}
       </div>
     </Container>

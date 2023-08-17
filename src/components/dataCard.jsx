e5163d633a6eaacc2b65724235ec8f22a922738c
Azam Gulov <gulovazam@gmail.com>
2023-08-17 18:49:33 +0500

    Add header container
    add content container

--- src/components/dataCard.jsx
+++ src/components/dataCard.jsx
@@ -3,31 +3,41 @@ export default function DataCard({
   onAdd,
   onDelete,
   onEdit,
+  onShow,
   expandable,
   buttonTitle,
   className,
+  title,
   containerState,
 }) {
   return (
     <div className={`data-section ${className}`} aria-expanded={containerState}>
-      {info.length > 0 && (
-        <ul>
-          {info.map((i) => (
-            <li key={i.id}>
-              <button type="button" onClick={() => onEdit(i.id)}>
-                {i.name ??
-                  `${i.firstName ?? "First and"} ${i.lastName ?? "Last Name"}`}
-              </button>
-              {expandable && (
-                <button type="button" onClick={() => onDelete(i.id)}>
-                  Delete
+      <div className="container-header">
+        <h2 className="container-title">{title}</h2>
+        <button type="button" onClick={onShow}></button>
+      </div>
+      <div className="container-content">
+        {info.length > 0 && (
+          <ul>
+            {info.map((i) => (
+              <li key={i.id}>
+                <button type="button" onClick={() => onEdit(i.id)}>
+                  {i.name ??
+                    `${i.firstName ?? "First and"} ${
+                      i.lastName ?? "Last Name"
+                    }`}
                 </button>
-              )}
-            </li>
-          ))}
-        </ul>
-      )}
-      {expandable && <AddNewButton onAdd={onAdd} title={buttonTitle} />}
+                {expandable && (
+                  <button type="button" onClick={() => onDelete(i.id)}>
+                    Delete
+                  </button>
+                )}
+              </li>
+            ))}
+          </ul>
+        )}
+        {expandable && <AddNewButton onAdd={onAdd} title={buttonTitle} />}
+      </div>
     </div>
   );
 }
--- src/components/inputCard.jsx
+++ src/components/inputCard.jsx
@@ -3,8 +3,10 @@ import Forms from "./forms";
 
 export default function InputCard({
   className,
+  title,
   onSubmit,
   onCancel,
+  onShow,
   containerState,
   sectionForm,
   information,
@@ -21,25 +23,33 @@ export default function InputCard({
       onSubmit={onSubmit}
       aria-expanded={containerState}
     >
-      {Forms[sectionForm].map((input) => (
-        <InputField
-          key={input.title}
-          type={input.type ?? "text"}
-          title={input.title}
-          placeholder={input.placeholder}
-          infoState={information}
-          setInfoState={setInformation}
-          propToUpdate={input.propToUpdate}
-          itemId={
-            itemsId.editItemId === null ? itemsId.newItemId : itemsId.editItemId
-          }
-          value={editItem && editItem[input.propToUpdate]}
-        />
-      ))}
-      <button>Save</button>
-      <button type="button" onClick={onCancel}>
-        Cancel
-      </button>
+      <div className="container-header">
+        <h2 className="container-title">{title}</h2>
+        <button type="button" onClick={onShow}></button>
+      </div>
+      <div className="container-content">
+        {Forms[sectionForm].map((input) => (
+          <InputField
+            key={input.title}
+            type={input.type ?? "text"}
+            title={input.title}
+            placeholder={input.placeholder}
+            infoState={information}
+            setInfoState={setInformation}
+            propToUpdate={input.propToUpdate}
+            itemId={
+              itemsId.editItemId === null
+                ? itemsId.newItemId
+                : itemsId.editItemId
+            }
+            value={editItem && editItem[input.propToUpdate]}
+          />
+        ))}
+        <button>Save</button>
+        <button type="button" onClick={onCancel}>
+          Cancel
+        </button>
+      </div>
     </form>
   );
 }

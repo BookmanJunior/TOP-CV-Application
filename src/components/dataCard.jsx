9468fb0132dd8de2bd7c0afba29ae335e0c7776c
Azam Gulov <gulovazam@gmail.com>
2023-08-18 15:52:32 +0500

    Remove container head and container content

--- src/components/dataCard.jsx
+++ src/components/dataCard.jsx
@@ -3,41 +3,29 @@ export default function DataCard({
   onAdd,
   onDelete,
   onEdit,
-  onShow,
   expandable,
   buttonTitle,
-  className,
-  title,
-  containerState,
 }) {
   return (
-    <div className={`data-section ${className}`} aria-expanded={containerState}>
-      <div className="container-header">
-        <h2 className="container-title">{title}</h2>
-        <button type="button" onClick={onShow}></button>
-      </div>
-      <div className="container-content">
-        {info.length > 0 && (
-          <ul>
-            {info.map((i) => (
-              <li key={i.id}>
-                <button type="button" onClick={() => onEdit(i.id)}>
-                  {i.name ??
-                    `${i.firstName ?? "First and"} ${
-                      i.lastName ?? "Last Name"
-                    }`}
+    <div>
+      {info.length > 0 && (
+        <ul>
+          {info.map((i) => (
+            <li key={i.id}>
+              <button type="button" onClick={() => onEdit(i.id)}>
+                {i.name ??
+                  `${i.firstName ?? "First and"} ${i.lastName ?? "Last Name"}`}
+              </button>
+              {expandable && (
+                <button type="button" onClick={() => onDelete(i.id)}>
+                  Delete
                 </button>
-                {expandable && (
-                  <button type="button" onClick={() => onDelete(i.id)}>
-                    Delete
-                  </button>
-                )}
-              </li>
-            ))}
-          </ul>
-        )}
-        {expandable && <AddNewButton onAdd={onAdd} title={buttonTitle} />}
-      </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
+      {expandable && <AddNewButton onAdd={onAdd} title={buttonTitle} />}
     </div>
   );
 }
--- src/components/inputCard.jsx
+++ src/components/inputCard.jsx
@@ -2,12 +2,8 @@ import InputField from "./inputField";
 import Forms from "./forms";
 
 export default function InputCard({
-  className,
-  title,
   onSubmit,
   onCancel,
-  onShow,
-  containerState,
   sectionForm,
   information,
   setInformation,
@@ -18,38 +14,26 @@ export default function InputCard({
   )[0];
 
   return (
-    <form
-      className={`form-section ${className}`}
-      onSubmit={onSubmit}
-      aria-expanded={containerState}
-    >
-      <div className="container-header">
-        <h2 className="container-title">{title}</h2>
-        <button type="button" onClick={onShow}></button>
-      </div>
-      <div className="container-content">
-        {Forms[sectionForm].map((input) => (
-          <InputField
-            key={input.title}
-            type={input.type ?? "text"}
-            title={input.title}
-            placeholder={input.placeholder}
-            infoState={information}
-            setInfoState={setInformation}
-            propToUpdate={input.propToUpdate}
-            itemId={
-              itemsId.editItemId === null
-                ? itemsId.newItemId
-                : itemsId.editItemId
-            }
-            value={editItem && editItem[input.propToUpdate]}
-          />
-        ))}
-        <button>Save</button>
-        <button type="button" onClick={onCancel}>
-          Cancel
-        </button>
-      </div>
+    <form onSubmit={onSubmit}>
+      {Forms[sectionForm].map((input) => (
+        <InputField
+          key={input.title}
+          type={input.type ?? "text"}
+          title={input.title}
+          placeholder={input.placeholder}
+          infoState={information}
+          setInfoState={setInformation}
+          propToUpdate={input.propToUpdate}
+          itemId={
+            itemsId.editItemId === null ? itemsId.newItemId : itemsId.editItemId
+          }
+          value={editItem && editItem[input.propToUpdate]}
+        />
+      ))}
+      <button>Save</button>
+      <button type="button" onClick={onCancel}>
+        Cancel
+      </button>
     </form>
   );
 }

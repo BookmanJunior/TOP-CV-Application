8013244baf0607cdbf6ad33fe9f380e26ac06ed9
Azam Gulov <gulovazam@gmail.com>
2023-08-22 20:38:07 +0500

    add functionality to add bullet points

--- src/components/inputField.jsx
+++ src/components/inputField.jsx
@@ -1,3 +1,5 @@
+import { v4 as uuid } from "uuid";
+
 export default function InputField({
   type = "text",
   title,
@@ -13,6 +15,20 @@ export default function InputField({
       ? title.split(" ")[0].toLowerCase() + title.split(" ")[1]
       : title;
 
+  const newBulletPoint = { id: uuid(), point: "" };
+
+  const handleMoreBulletPoints = () => {
+    setInfoState(
+      infoState.map((item) => {
+        if (item.id === itemId) {
+          const updatedDetails = [...item.details, newBulletPoint];
+          return { ...item, details: updatedDetails };
+        }
+        return { ...item };
+      })
+    );
+  };
+
   const handleChange = (e, updateProp) => {
     setInfoState(
       infoState.map((item) => {
@@ -24,10 +40,63 @@ export default function InputField({
     );
   };
 
+  const handleBulletPointChange = (e, pointItemId) => {
+    const infoObject = infoState.filter((item) => item.id === itemId)[0];
+    const updatedObject = infoObject.details.map((item) => {
+      if (item.id === pointItemId) {
+        return { ...item, point: e.target.value };
+      }
+      return { ...item };
+    });
+    setInfoState(
+      infoState.map((item) => {
+        if (item.id === itemId) {
+          return { ...item, details: updatedObject };
+        }
+        return { ...item };
+      })
+    );
+  };
+
+  const handleDelete = (bulletPointItemId) => {
+    const infoObject = infoState.filter((item) => item.id === itemId)[0];
+    const updatedObject = infoObject.details.filter(
+      (item) => item.id !== bulletPointItemId
+    );
+    setInfoState(
+      infoState.map((item) => {
+        if (item.id === itemId) {
+          return { ...item, details: updatedObject };
+        }
+        return { ...item };
+      })
+    );
+  };
+
   return (
     <label>
       {title}
-      {type !== "textarea" ? (
+      {title === "Details" ? (
+        infoState.map(
+          (item) =>
+            item.details &&
+            item.details.map((point) => (
+              <>
+                <input
+                  key={point.id}
+                  type={type}
+                  name={name}
+                  value={value}
+                  placeholder={placeholder}
+                  onInput={(e) => handleBulletPointChange(e, point.id)}
+                />
+                <button type="button" onClick={() => handleDelete(point.id)}>
+                  Delete
+                </button>
+              </>
+            ))
+        )
+      ) : (
         <input
           type={type}
           name={name}
@@ -36,14 +105,11 @@ export default function InputField({
           onInput={(e) => handleChange(e, propToUpdate)}
           required
         />
-      ) : (
-        <textarea
-          name={name}
-          value={value}
-          placeholder={placeholder}
-          onInput={(e) => handleChange(e, propToUpdate)}
-          required
-        />
+      )}
+      {title === "Details" && (
+        <button type="button" onClick={handleMoreBulletPoints}>
+          Add More
+        </button>
       )}
     </label>
   );

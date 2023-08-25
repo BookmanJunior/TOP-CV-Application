6a0f4c7adfa1ca826f56f2c17066e467b54b7b5e
Azam Gulov <gulovazam@gmail.com>
2023-08-25 18:25:34 +0500

    Code refactor
    add header, rightside and leftside container components
    add bulletpoints component

--- src/components/outputCard.jsx
+++ src/components/outputCard.jsx
@@ -1,8 +1,56 @@
-export default function OutputCard({ title, className, children }) {
+export default function OutputCard({ title, className, info }) {
   return (
     <div className={`output-card ${className}`}>
-      {title && <h3 className="output-card-title">{title}</h3>}
-      {children}
+      {info.length > 0 && <Header title={title} />}
+      {info.map((i) => (
+        <div className="output-content-container" key={i.id}>
+          <LeftSideContainer info={i} />
+          {i.details && <BulletPoints info={i} />}
+          <RightSideContainer info={i} />
+        </div>
+      ))}
     </div>
+  );
+}
+
+function Header({ title = "" }) {
+  return title && <h3 className="output-card-title">{title}</h3>;
+}
+
+function LeftSideContainer({ info }) {
+  return (
+    <div className="left-side-content-container">
+      <BoldParagraph>{info.name}</BoldParagraph>
+      <ItalicParagraph>{info.degree ?? info.jobTitle}</ItalicParagraph>
+    </div>
+  );
+}
+
+function RightSideContainer({ info }) {
+  const fullDate = `${info.startDate ?? ""} - ${info.endDate ?? ""}`;
+  return (
+    <div className="right-side-content-container">
+      <BoldParagraph>{info.startDate && fullDate}</BoldParagraph>
+      <ItalicParagraph>{info.location ?? info.technologies}</ItalicParagraph>
+    </div>
+  );
+}
+
+function BoldParagraph({ children }) {
+  return <p className="text-bold">{children}</p>;
+}
+
+function ItalicParagraph({ children }) {
+  return <p className="text-italic">{children}</p>;
+}
+
+function BulletPoints({ info }) {
+  return info.details.map(
+    (bulletPoint) =>
+      bulletPoint.point && (
+        <ul key={bulletPoint.id} className="output-bullet-points">
+          <li className="bullet-point">{bulletPoint.point}</li>
+        </ul>
+      )
   );
 }

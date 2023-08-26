58fb1480c9be88d807af2e18cc44e85e5417a45b
Azam Gulov <gulovazam@gmail.com>
2023-08-26 20:06:30 +0500

    Initial commit

---
+++ src/components/outputHeader.jsx
@@ -0,0 +1,34 @@
+export default function OutputHeader({ generalInformation }) {
+  return (
+    <div className="output-header">
+      <HeaderTitle generalInformation={generalInformation} />
+      <HeaderContent generalInformation={generalInformation} />
+      <Links generalInformation={generalInformation} />
+    </div>
+  );
+}
+
+function HeaderTitle({ generalInformation }) {
+  return generalInformation.map((item) => (
+    <h1 key={item.firstName}>{`${item.firstName} ${item.lastName}`}</h1>
+  ));
+}
+
+function HeaderContent({ generalInformation }) {
+  return generalInformation.map((item) => (
+    <div key={item.id} className="header-general-content">
+      <p>{item.email}</p>
+      <p>{item.phoneNumber}</p>
+      <p>{item.location}</p>
+    </div>
+  ));
+}
+
+function Links({ generalInformation }) {
+  return generalInformation.map((item) => (
+    <div className="header-links" key={"links"}>
+      <p>{item.linkedin}</p>
+      <p>{item.github}</p>
+    </div>
+  ));
+}

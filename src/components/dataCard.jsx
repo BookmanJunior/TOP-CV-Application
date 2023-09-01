5bb6c5597e1226226e64bd69127a8a0f4c70786c
Azam Gulov <gulovazam@gmail.com>
2023-09-01 18:14:42 +0500

    remove expandable prop
    remove AddNewButton component
    code refactor

--- src/components/dataCard.jsx
+++ src/components/dataCard.jsx
@@ -1,46 +1,27 @@
 import { ReactComponent as Delete } from "../assets/delete-icon.svg";
 
-export default function DataCard({
-  info,
-  onAdd,
-  onDelete,
-  onEdit,
-  expandable,
-  buttonTitle,
-}) {
+export default function DataCard({ info, onDelete, onEdit }) {
   return (
     <>
       {info.length > 0 && (
         <ul>
-          {info.map((i) => (
-            <li key={i.id}>
-              <button type="button" onClick={() => onEdit(i.id)}>
-                {i.name ??
-                  `${i.firstName ?? "First and"} ${i.lastName ?? "Last Name"}`}
-              </button>
-              {expandable && (
+          {info.map((i) => {
+            const fullName = `${i.firstName ?? "Edit"} ${
+              i.lastName ?? "Details"
+            }`;
+            return (
+              <li key={i.id}>
+                <button type="button" onClick={() => onEdit(i.id)}>
+                  {i.name ?? fullName}
+                </button>
                 <button type="button" onClick={() => onDelete(i.id)}>
                   <Delete />
                 </button>
-              )}
-            </li>
-          ))}
+              </li>
+            );
+          })}
         </ul>
       )}
-      {expandable && <AddNewButton onAdd={onAdd} title={buttonTitle} />}
     </>
-  );
-}
-
-function AddNewButton({ onAdd, title }) {
-  return (
-    <button
-      className="add-button primary-color"
-      key={title}
-      type="button"
-      onClick={onAdd}
-    >
-      {title}
-    </button>
   );
 }

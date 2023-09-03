a5c2d131357f40b7f68e1eb917cfcc2c46b22a51
Azam Gulov <gulovazam@gmail.com>
2023-09-03 17:41:47 +0500

    add onclick listener to modal container

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -4,6 +4,11 @@ export default function Modal({ isActive, setIsActive, cb }) {
       className={`modal-container ${
         isActive ? "modal-active" : "modal-closed"
       }`}
+      onClick={(e) => {
+        if (e.target.matches(".modal-container")) {
+          setIsActive(!isActive);
+        }
+      }}
     >
       <div className="modal-content">
         <p className="text-bold">Are you sure you want to erase all data?</p>
